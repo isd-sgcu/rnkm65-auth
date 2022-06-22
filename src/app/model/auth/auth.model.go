4bc3ad9de7606340ea068e776f654b163603da96
samithiwat <samithiwat2544@gmail.com>
2022-06-23 04:00:05 +0700

    Add index to user id

--- src/app/model/auth/auth.model.go
+++ src/app/model/auth/auth.model.go
@@ -6,7 +6,7 @@ import (
 
 type Auth struct {
 	model.Base
-	UserID       string `json:"user_id" gorm:"type:tinytext"`
+	UserID       string `json:"user_id" gorm:"index:,unique"`
 	Role         string `json:"role" gorm:"type:tinytext"`
 	RefreshToken string `json:"refresh_token" gorm:"index"`
 }
