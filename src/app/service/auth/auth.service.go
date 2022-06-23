bd4ab88b235530dbc30f4f5b2f818927b7317092
samithiwat <samithiwat2544@gmail.com>
2022-06-24 02:34:33 +0700

    Implement the auth service

--- src/app/service/auth/auth.service.go
+++ src/app/service/auth/auth.service.go
@@ -2,26 +2,121 @@ package auth
 
 import (
 	"context"
+	dto "github.com/isd-sgcu/rnkm65-auth/src/app/dto/auth"
 	model "github.com/isd-sgcu/rnkm65-auth/src/app/model/auth"
+	"github.com/isd-sgcu/rnkm65-auth/src/app/utils"
+	"github.com/isd-sgcu/rnkm65-auth/src/constant"
 	"github.com/isd-sgcu/rnkm65-auth/src/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Service struct {
-	repo IRepository
+	repo           IRepository
+	chulaSSOClient IChulaSSOClient
+	tokenService   ITokenService
+	userService    IUserService
 }
 
 type IRepository interface {
-	FindByStudentID(string, *model.Auth) error
 	FindByRefreshToken(string, *model.Auth) error
+	FindByUserID(string, *model.Auth) error
 	Create(*model.Auth) error
+	Update(string, *model.Auth) error
 }
 
-func NewService(repo IRepository) *Service {
-	return &Service{repo: repo}
+type IChulaSSOClient interface {
+	VerifyTicket(string, *dto.ChulaSSOCredential) error
 }
 
-func (s *Service) VerifyTicket(context.Context, *proto.VerifyTicketRequest) (res *proto.VerifyTicketResponse, err error) {
-	return
+type IUserService interface {
+	FindByStudentID(string) (*proto.User, error)
+	Create(*proto.User) (*proto.User, error)
+}
+
+type ITokenService interface {
+	CreateOrUpdateCredentials(*model.Auth) (*proto.Credential, error)
+}
+
+func NewService(
+	repo IRepository,
+	chulaSSOClient IChulaSSOClient,
+	tokenService ITokenService,
+	userService IUserService,
+) *Service {
+	return &Service{
+		repo:           repo,
+		chulaSSOClient: chulaSSOClient,
+		tokenService:   tokenService,
+		userService:    userService,
+	}
+}
+
+func (s *Service) VerifyTicket(_ context.Context, req *proto.VerifyTicketRequest) (res *proto.VerifyTicketResponse, err error) {
+	ssoData := dto.ChulaSSOCredential{}
+	auth := model.Auth{}
+
+	err = s.chulaSSOClient.VerifyTicket(req.Ticket, &ssoData)
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, err.Error())
+	}
+
+	user, err := s.userService.FindByStudentID(ssoData.Ouid)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			switch st.Code() {
+			case codes.NotFound:
+				year, err := utils.CalYearFromID(ssoData.Ouid)
+				if err != nil {
+					return nil, err
+				}
+
+				faculty, err := utils.GetFacultyFromID(ssoData.Ouid)
+				if err != nil {
+					return nil, err
+				}
+
+				in := &proto.User{
+					StudentID: ssoData.Ouid,
+					Year:      year,
+					Faculty:   faculty.FacultyEN,
+				}
+
+				user, err = s.userService.Create(in)
+				if err != nil {
+					return nil, status.Error(codes.Unauthenticated, st.Message())
+				}
+
+				auth = model.Auth{
+					Role:   constant.USER,
+					UserID: user.Id,
+				}
+
+				err = s.repo.Create(&auth)
+				if err != nil {
+					return nil, status.Error(codes.Unavailable, st.Message())
+				}
+
+			default:
+				return nil, status.Error(codes.Unavailable, st.Message())
+			}
+		} else {
+			return nil, status.Error(codes.Unavailable, "Service is down")
+		}
+	} else {
+		err := s.repo.FindByUserID(user.Id, &auth)
+		if err != nil {
+			return nil, status.Error(codes.NotFound, "not found user")
+		}
+	}
+
+	credential, err := s.tokenService.CreateOrUpdateCredentials(&auth)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &proto.VerifyTicketResponse{Credential: credential}, err
 }
 
 func (s *Service) Validate(context.Context, *proto.ValidateRequest) (res *proto.ValidateResponse, err error) {
--- src/app/service/auth/auth.service_test.go
+++ src/app/service/auth/auth.service_test.go
@@ -1,14 +1,200 @@
 package auth
 
 import (
+	"context"
+	"github.com/bxcodec/faker/v3"
+	"github.com/google/uuid"
+	dto "github.com/isd-sgcu/rnkm65-auth/src/app/dto/auth"
+	"github.com/isd-sgcu/rnkm65-auth/src/app/model"
+	"github.com/isd-sgcu/rnkm65-auth/src/app/model/auth"
+	"github.com/isd-sgcu/rnkm65-auth/src/constant"
+	mock "github.com/isd-sgcu/rnkm65-auth/src/mocks/auth"
+	"github.com/isd-sgcu/rnkm65-auth/src/proto"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
 	"testing"
+	"time"
 )
 
 type AuthServiceTest struct {
 	suite.Suite
+	Auth            *auth.Auth
+	UserDto         *proto.User
+	Credential      *proto.Credential
+	UnauthorizedErr error
+	NotFoundErr     error
+	ServiceDownErr  error
 }
 
 func TestAuthService(t *testing.T) {
 	suite.Run(t, new(AuthServiceTest))
+}
+
+func (t *AuthServiceTest) SetupTest() {
+	t.Auth = &auth.Auth{
+		Base: model.Base{
+			ID:        uuid.New(),
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+			DeletedAt: gorm.DeletedAt{},
+		},
+		UserID:       faker.UUIDDigit(),
+		Role:         constant.USER,
+		RefreshToken: faker.Word(),
+	}
+
+	t.UserDto = &proto.User{
+		Id:                    t.Auth.UserID,
+		Firstname:             faker.FirstName(),
+		Lastname:              faker.LastName(),
+		Nickname:              faker.Name(),
+		StudentID:             "63xxxxxx21",
+		Faculty:               "Faculty of Engineering",
+		Year:                  "3",
+		Phone:                 faker.Phonenumber(),
+		LineID:                faker.Word(),
+		Email:                 faker.Email(),
+		AllergyFood:           faker.Word(),
+		FoodRestriction:       faker.Word(),
+		AllergyMedicine:       faker.Word(),
+		Disease:               faker.Word(),
+		VaccineCertificateUrl: faker.URL(),
+		ImageUrl:              faker.URL(),
+	}
+
+	t.Credential = &proto.Credential{
+		AccessToken:  faker.Word(),
+		RefreshToken: t.Auth.RefreshToken,
+		ExpiresIn:    3600,
+	}
+
+	t.UnauthorizedErr = errors.New("unauthorized")
+	t.NotFoundErr = errors.New("not found user")
+	t.ServiceDownErr = errors.New("service is down")
+}
+
+func (t *AuthServiceTest) TestVerifyTicketSuccessFirstTimeLogin() {
+	want := &proto.VerifyTicketResponse{
+		Credential: t.Credential,
+	}
+
+	ticket := faker.Word()
+	chulaSSORes := &dto.ChulaSSOCredential{
+		UID:         faker.Word(),
+		Username:    faker.Username(),
+		Gecos:       faker.Username(),
+		Email:       faker.Email(),
+		Disable:     false,
+		Roles:       []string{"student"},
+		Firstname:   faker.FirstName(),
+		Lastname:    faker.LastName(),
+		FirstnameTH: faker.FirstName(),
+		LastnameTH:  faker.LastName(),
+		Ouid:        t.UserDto.StudentID,
+	}
+
+	a := &auth.Auth{
+		UserID: t.UserDto.Id,
+		Role:   t.Auth.Role,
+	}
+
+	repo := &mock.RepositoryMock{}
+	repo.On("Create", a).Return(t.Auth, nil)
+
+	chulaSSOClient := &mock.ChulaSSOClientMock{}
+	chulaSSOClient.On("VerifyTicket", ticket, &dto.ChulaSSOCredential{}).Return(chulaSSORes, nil)
+
+	in := &proto.User{
+		StudentID: t.UserDto.StudentID,
+		Faculty:   t.UserDto.Faculty,
+		Year:      t.UserDto.Year,
+	}
+
+	userService := &mock.UserServiceMock{}
+	userService.On("FindByStudentID", t.UserDto.StudentID).Return(nil, status.Error(codes.NotFound, "not found user"))
+	userService.On("Create", in).Return(t.UserDto, nil)
+
+	tokenService := &mock.TokenServiceMock{}
+	tokenService.On("CreateOrUpdateCredentials", t.Auth).Return(t.Credential, nil)
+
+	srv := NewService(repo, chulaSSOClient, tokenService, userService)
+	actual, err := srv.VerifyTicket(context.Background(), &proto.VerifyTicketRequest{Ticket: ticket})
+
+	assert.Nil(t.T(), err)
+	assert.Equal(t.T(), want, actual)
+}
+
+func (t *AuthServiceTest) TestVerifyTicketSuccessNotFirstTimeLogin() {
+	ticket := faker.Word()
+
+	repo := &mock.RepositoryMock{}
+	repo.On("FindByUserID", t.UserDto.Id).Return(t.Auth, nil)
+
+	chulaSSOClient := &mock.ChulaSSOClientMock{}
+	chulaSSOClient.On("VerifyTicket", ticket, &dto.ChulaSSOCredential{}).Return(nil, errors.New("Invalid Ticket"))
+
+	userService := &mock.UserServiceMock{}
+	userService.On("FindByStudentID", t.UserDto.Id).Return(t.UserDto, nil)
+
+	tokenService := &mock.TokenServiceMock{}
+	tokenService.On("CreateOrUpdateCredentials", t.Auth).Return(t.Credential, nil)
+
+	srv := NewService(repo, chulaSSOClient, tokenService, userService)
+	actual, err := srv.VerifyTicket(context.Background(), &proto.VerifyTicketRequest{Ticket: ticket})
+
+	st, ok := status.FromError(err)
+
+	assert.True(t.T(), ok)
+	assert.Nil(t.T(), actual)
+	assert.Equal(t.T(), codes.Unauthenticated, st.Code())
+}
+
+func (t *AuthServiceTest) TestVerifyTicketInvalid() {
+	ticket := faker.Word()
+
+	repo := &mock.RepositoryMock{}
+
+	chulaSSOClient := &mock.ChulaSSOClientMock{}
+	chulaSSOClient.On("VerifyTicket", ticket, &dto.ChulaSSOCredential{}).Return(nil, errors.New("Invalid Ticket"))
+
+	userService := &mock.UserServiceMock{}
+	userService.On("FindByStudentID", t.UserDto.Id).Return(nil, t.NotFoundErr)
+
+	tokenService := &mock.TokenServiceMock{}
+
+	srv := NewService(repo, chulaSSOClient, tokenService, userService)
+	actual, err := srv.VerifyTicket(context.Background(), &proto.VerifyTicketRequest{Ticket: ticket})
+
+	st, ok := status.FromError(err)
+
+	assert.True(t.T(), ok)
+	assert.Nil(t.T(), actual)
+	assert.Equal(t.T(), codes.Unauthenticated, st.Code())
+}
+
+func (t *AuthServiceTest) TestVerifyTicketGrpcErr() {
+	ticket := faker.Word()
+
+	repo := &mock.RepositoryMock{}
+
+	chulaSSOClient := &mock.ChulaSSOClientMock{}
+	chulaSSOClient.On("VerifyTicket", ticket, &dto.ChulaSSOCredential{}).Return(nil, errors.New("Invalid Ticket"))
+
+	userService := &mock.UserServiceMock{}
+	userService.On("FindByStudentID", t.UserDto.Id).Return(nil, t.NotFoundErr)
+
+	tokenService := &mock.TokenServiceMock{}
+
+	srv := NewService(repo, chulaSSOClient, tokenService, userService)
+	actual, err := srv.VerifyTicket(context.Background(), &proto.VerifyTicketRequest{Ticket: ticket})
+
+	st, ok := status.FromError(err)
+
+	assert.True(t.T(), ok)
+	assert.Nil(t.T(), actual)
+	assert.Equal(t.T(), codes.Unauthenticated, st.Code())
 }
